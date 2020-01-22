5d2f0cc81d87e565f6776fb5f957a725b6fef1ed
Alexander J Groszewski <agroszewski@bowser.ece.utexas.edu>
2020-01-21 23:15:38 -0600

    get csv writer working for area

--- dsc/arch_sweep/script/write_csv.py
+++ dsc/arch_sweep/script/write_csv.py
@@ -5,24 +5,55 @@ import os
 import subprocess
 
 header_inputs = ["",2,'','','','','','','','',3,'','','','','','','','',4,'','','','','','','','',5,'','','','','','','','']
-header_bits   = [2,3,4 ,5 ,6 ,7 ,8 ,9 ,10]*4
+header_bits   = [""] + [2,3,4 ,5 ,6 ,7 ,8 ,9 ,10]*4
+designs       = ["dsc_serial_mul","es_naive_mul","ms_serial_by2_mul"]
+
 cwd = os.getcwd()
 
-dsc_serial_mul_area_tot = ["dsc_serial_mul"]
-
-for i in range(2,5+1):
-    for d in range(2,10+1):
-        arch = "i" + str(i) + "d" + str(d)
-        area_rpt = os.path.join(cwd,'build','dsc_serial_mul',arch,'synth','area.rpt')
-        cmd = 'grep -A2 Instance ' + area_rpt + '| tail -n1 | awk \'{print $3}\''
-        test = os.popen(cmd).read().rstrip()
-        print test
-        dsc_serial_mul_area_tot.append(test)
+area_tot   = []
+area_gates = []
+power      = []
+latency    = []
 
 
-with open('csv/test.csv', mode='w') as test_csv:
+for arch in designs:
+    area_tot_temp = []
+    area_gates_temp = []
+    area_tot_temp.append(arch)
+    area_gates_temp.append(arch)
+    for i in range(2,5+1):
+        for d in range(2,10+1):
+            flavor = "i" + str(i) + "d" + str(d)
+            area_rpt = os.path.join(cwd,'build',arch,flavor,'synth','area.rpt')
+            cmd_area_tot = 'grep -A2 Instance ' + area_rpt + '| tail -n1 | awk \'{print $3}\''
+            area_tot_grep = os.popen(cmd_area_tot).read().rstrip()
+            area_tot_temp.append(area_tot_grep)
+            cmd_area_gates = 'grep -A2 Instance ' + area_rpt + '| tail -n1 | awk \'{print $2}\''
+            area_gates_grep = os.popen(cmd_area_gates).read().rstrip()
+            area_gates_temp.append(area_gates_grep)
+    area_tot.append(area_tot_temp)
+    area_gates.append(area_gates_temp)
+
+
+with open('csv/archsweep_Area.csv', mode='w') as test_csv:
     csv_write = csv.writer(test_csv,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+    csv_write.writerow(['Total Area'])
     csv_write.writerow(header_inputs)
     csv_write.writerow(header_bits)
-    csv_write.writerow(dsc_serial_mul_area_tot)
-    #csv_write.write_row([2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10])
+    for row in area_tot:
+        csv_write.writerow(row)
+    csv_write.writerow([''])
+    csv_write.writerow(['Number of Gates'])
+    csv_write.writerow(header_inputs)
+    csv_write.writerow(header_bits)
+    for row in area_gates:
+        csv_write.writerow(row)
+
+
+with open('csv/archsweep_latency.csv', mode='w') as test_csv:
+    csv_write = csv.writer(test_csv,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+    csv_write.writerow(['Latency'])
+    csv_write.writerow(header_inputs)
+    csv_write.writerow(header_bits)
+    for row in area_tot:
+        csv_write.writerow(row)
