b564aec7aee01f85f519d662f0576fce46d68724
Alexander J Groszewski <agroszewski@bowser.ece.utexas.edu>
2020-09-05 14:39:02 -0500

    start tracking sc_serial_mul build dir

---
+++ dsc/arch_sweep/build/sc_serial_mul/i2d4/params.vh
@@ -0,0 +1,5 @@
+parameter DATA_WIDTH = 4;
+parameter NUM_INPUTS = 2;
+parameter WXIP1 = 9;
+parameter MIN_CYC_DSC = 256;
+
--- dsc/arch_sweep/script/setproj.sh
+++ dsc/arch_sweep/script/setproj.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 module load syn/vcs
-module load cadence/2016
+module load cadence/2018
 export MODEL_ROOT=$PWD
